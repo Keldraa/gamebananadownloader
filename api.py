44118fb778dd5aedba5144b45da275b0c9810286
Keldraistaken <greenblackarrow@gmail.com>
2022-01-09 21:45:01 +0300

    Final Commit

--- api.py
+++ api.py
@@ -1,14 +1,19 @@
-import os
 import json
-import requests
 import shutil
+from datetime import datetime
+
+import patoolib
+import requests
 import os
 
-from datetime import datetime
+# PATH = "/home/csgoservers/serverfiles4/css/maps/"
+PATH = "/home/dora/Desktop"
+
 
 class GamebananaAPI:
     def __init__(self, mod_id):
-        self.base_api = "https://gamebanana.com/apiv5/Mod/" + str(mod_id) + "?_csvProperties=_idRow,_sName,_aFiles,_aGame,_sName,_aPreviewMedia,_aSubmitter"
+        self.base_api = "https://gamebanana.com/apiv5/Mod/" + str(
+            mod_id) + "?_csvProperties=_idRow,_sName,_aFiles,_aGame,_sName,_aPreviewMedia,_aSubmitter"
 
     def get_json(self):
         url = self.base_api
@@ -18,17 +23,19 @@ class GamebananaAPI:
             return j
         except:
             return []
-    
+
+
 def get_date(timestamp):
     dt_object = datetime.fromtimestamp(timestamp)
 
     return dt_object
 
-def download_file(url, local_filename, mapFile):
-    with requests.get(url, stream=True) as r:
-        with open(mapFile, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
-            shutil.unpack_archive(mapFile)
 
-    os.remove(mapFile)        
+def download_file(url, local_filename, map_file):
+    with requests.get(url, stream=True) as r:
+        with open(map_file, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            patoolib.extract_archive(map_file, outdir=PATH)
+    os.remove(map_file)
+
     return local_filename
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import requests
 import discord
 from discord.ext import commands
+from hurry.filesize import size
 import api
 
 TOKEN = "OTI5Njg5NTc3MTY4NDQxMzk0.Ydq-4w.t075-NSdKUxHp7_aIbB_8koHgjI"
 client = commands.Bot(command_prefix='?')
-PATH = "/home/csgoservers/serverfiles4/css/maps/"
 
-@client.command(alias=["r", "rq", "dl"])
+
+@client.command(aliases=["r", "rq", "dl"])
 async def request(ctx, arg1):
     url = arg1
     mod_id = url.rsplit('/', 1)[-1]
 
     j = api.GamebananaAPI(mod_id).get_json()
 
+    if j["_aGame"]["_sAbbreviation"] == "CS:S":
 
-    if j["_aGame"]["_sName"] == "Counter-Strike: Source":
+        name = j["_sName"] or "Unknown"
+        creator = j["_aSubmitter"]["_sName"] or "Unknown"
+        creator_avatar_url = j["_aSubmitter"][
+                                 "_sAvatarUrl"] or "https://images.gamebanana.com/static/img/defaults/avatar.gif"
+        map_image = j["_aPreviewMedia"][0]["_sFile"] or ""
 
-        mapName = j["_sName"] or "Unknown"
-        mapCreator = j["_aSubmitter"]["_sName"] or "Unknown"
-        mapCreatorAvatarUrl = j["_aSubmitter"]["_sAvatarUrl"] or "https://images.gamebanana.com/static/img/defaults/avatar.gif"
-        mapDownloadUrl = j["_aFiles"][0]["_sDownloadUrl"] or ""
-        mapFile = j["_aFiles"][0]["_sFile"]
-        mapUploadDate = api.get_date(j["_aFiles"][0]["_tsDateAdded"])
+        download_url = j["_aFiles"][0]["_sDownloadUrl"]
+        map_file = j["_aFiles"][0]["_sFile"]
+        upload_date = api.get_date(j["_aFiles"][0]["_tsDateAdded"])
 
-        print(f"{mapName} by {mapCreator}")
-        print(mapUploadDate)
-        print(mapDownloadUrl)
+        api.download_file(download_url, name, map_file)
 
-        api.download_file(mapDownloadUrl, mapName)
+        embed = discord.Embed(title=f"{name} by {creator}",
+                              description=f"Upload Date: {upload_date} \n Download URL: {download_url}"
+                              )
+        embed.set_footer(text="Size: " + size(j["_aFiles"][0]["_nFilesize"]), icon_url=creator_avatar_url)
+        embed.set_image(url="https://images.gamebanana.com/img/ss/mods/" + map_image)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/icons/639948169643425792/ac76d8508b89af4403b01dd7893a4b14.webp")
+
+        await ctx.send(embed=embed)
 
     else:
         await ctx.send("Game must be css")
