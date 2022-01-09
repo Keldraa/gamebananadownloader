dbed7dff2899df537832b38af1740b770c6d3f8d
Nora <50810398+akanora@users.noreply.github.com>
2022-01-09 18:50:07 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 import os
 import json
 import requests
+import shutil
+import os
 
+from datetime import datetime
 
 class GamebananaAPI:
     def __init__(self, mod_id):
@@ -15,3 +18,17 @@ class GamebananaAPI:
             return j
         except:
             return []
+    
+def get_date(timestamp):
+    dt_object = datetime.fromtimestamp(timestamp)
+
+    return dt_object
+
+def download_file(url, local_filename, mapFile):
+    with requests.get(url, stream=True) as r:
+        with open(mapFile, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            shutil.unpack_archive(mapFile)
+
+    os.remove(mapFile)        
+    return local_filename
