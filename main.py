0d938a2e88352f9281e85ac349630b841029b648
Nora <50810398+akanora@users.noreply.github.com>
2022-01-11 12:46:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,62 @@
-import json
-import shutil
-from datetime import datetime
-
-from pyunpack import Archive
-import requests
-import os
-
-
-class GamebananaAPI:
-    def __init__(self, mod_id):
-        self.base_api = "https://gamebanana.com/apiv5/Mod/" + str(
-            mod_id) + "?_csvProperties=_idRow,_sName,_aFiles,_aGame,_sName,_aPreviewMedia,_aSubmitter"
-
-    def get_json(self):
-        url = self.base_api
-        try:
-            r = requests.get(self.base_api)
-            j = json.loads(r.content)
-            return j
-        except:
-            return []
+import discord
+from discord import Color
+from discord.ext import commands
+from hurry.filesize import size
+import api
+import yaml
 
 
-def get_date(timestamp):
-    dt_object = datetime.fromtimestamp(timestamp)
-
-    return dt_object
+with open("config.yml", "r") as file:
+    cfg = yaml.safe_load(file)
 
 
-def download_file(url, file, path):
-    with requests.get(url, stream=True) as r:
-        with open(file, 'wb') as f:
-            print(path)
-            shutil.copyfileobj(r.raw, f)
-        Archive(file).extractall(path)
+TOKEN = cfg['token']
+client = commands.Bot(command_prefix=cfg['prefix'])
 
-    os.remove(file)
+
+@client.command(aliases=["r", "rq", "dl"])
+async def request(ctx, arg1):
+    url = arg1
+    mod_id = url.rsplit('/', 1)[-1]
+
+    j = api.GamebananaAPI(mod_id).get_json()
+
+    if (cfg['game_check'] == 1 and j['_aGame']['_sAbbreviation'] in cfg['games']) or cfg['game_check'] == 0:
+
+        name = j["_sName"] or "Unknown"
+        creator = j["_aSubmitter"]["_sName"] or "Unknown"
+        creator_avatar_url = j["_aSubmitter"][
+                                 "_sAvatarUrl"] or "https://images.gamebanana.com/static/img/defaults/avatar.gif"
+        map_image = j["_aPreviewMedia"][0]["_sFile"] or ""
+
+        download_url = j["_aFiles"][0]["_sDownloadUrl"]
+        map_file = j["_aFiles"][0]["_sFile"]
+        upload_date = api.get_date(j["_aFiles"][0]["_tsDateAdded"])
+
+        api.download_file(url=download_url, file=map_file, path=cfg['paths'][j['_aGame']['_sAbbreviation']], name)
+
+        embed = discord.Embed(title=f"{name} by {creator}",
+                              description=f"Upload Date: {upload_date} \n Download URL: {download_url} \n "
+                                          f"Game: {j['_aGame']['_sAbbreviation']}",
+                              colour=Color.blurple()
+                              )
+        embed.set_footer(text="Size: " + size(j["_aFiles"][0]["_nFilesize"]),
+                         icon_url="https://cdn.discordapp.com/icons/"
+                                  "639948169643425792/ac76d8508b89af4403b01dd7893a4b14.webp")
+        embed.set_image(url=f'https://images.gamebanana.com/img/ss/mods/{map_image}')
+        embed.set_thumbnail(url=creator_avatar_url)
+
+        message = await ctx.send(embed=embed)
+        await message.add_reaction('✅')
+
+    else:
+        await ctx.send(f"{j['_aGame']['_sAbbreviation']} is not listed under supported games in config.yml")
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as: {client.user}')
+
+
+client.remove_command("help")
+client.run(TOKEN)
